6296c572c43e47a6a627646c7065dc37b7a718e2
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2026-07-19 01:58:27 -0700

    replace default template code with proper application header

---
+++ src/components/app-ui/AppHeader.tsx
@@ -0,0 +1,9 @@
+function AppHeader() {
+  return (
+    <h1 className="border-b border-border px-6 py-4 font-heading text-base font-medium">
+      Launchers
+    </h1>
+  );
+}
+
+export default AppHeader;
