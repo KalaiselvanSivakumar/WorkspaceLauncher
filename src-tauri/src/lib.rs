70206b77db37f1ea6d671956fcd2242c971e3bcf
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2026-07-19 16:35:44 -0700

    move commands to separate module

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,57 +1,14 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
-use tauri::{AppHandle, Manager};
-
-use crate::models::AppStateData;
+pub mod commands;
 
 pub mod models;
-
-#[tauri::command]
-async fn get_application_data(app_handle: AppHandle) -> Result<AppStateData, String> {
-    // 1. Retrieve the application data directory
-    let mut path = match app_handle.path().app_data_dir() {
-        Ok(dir) => dir,
-        Err(e) => {
-            eprintln!("[Error] Failed to get application data directory: {}", e);
-            return Err(format!("Failed to get application data directory: {}", e));
-        }
-    };
-
-    path.push("app_state.json");
-
-    // 2. Check if the file exists
-    if !path.exists() {
-        println!("[Info] app_state.json does not exist yet. Returning empty object.");
-        return Ok(AppStateData::default());
-    }
-
-    // 3. Read the file contents
-    let file_content = match std::fs::read_to_string(&path) {
-        Ok(content) => content,
-        Err(e) => {
-            eprintln!("[Error] Failed to read file at {:?}: {}", path, e);
-            return Err(format!("Failed to read application data file: {}", e));
-        }
-    };
-
-    // 4. Parse the string into valid JSON
-    match serde_json::from_str::<AppStateData>(&file_content) {
-        Ok(json) => Ok(json),
-        Err(e) => {
-            eprintln!(
-                "[Error] Failed to parse JSON from file: {}. File might be corrupted.",
-                e
-            );
-            Err(format!("Application data file is corrupted: {}", e))
-        }
-    }
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_application_data])
+        .invoke_handler(tauri::generate_handler![commands::get_application_data])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
