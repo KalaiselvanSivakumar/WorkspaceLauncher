9f628c9d7349d399b3a1f910a9f14ad09ae1352d
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2026-07-23 01:49:35 -0700

    implement create workspace UI button component to separate functionality

---
+++ src/components/app/CreateWorkspaceButton.tsx
@@ -0,0 +1,15 @@
+import { PlusIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import { useUIStore } from "@/stores/ui-store";
+
+function CreateWorkspaceButton() {
+  const showCreate = useUIStore((state) => state.showCreate);
+
+  return (
+    <Button type="button" className="gap-1" onClick={() => showCreate()}>
+      <PlusIcon /> Create Workspace
+    </Button>
+  );
+}
+
+export default CreateWorkspaceButton;
