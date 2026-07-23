e575a774059edc0b81c155d38449ef620dd601a0
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2026-07-23 02:15:59 -0700

    support go back action in the page header

--- src/components/app/PageHeader.tsx
+++ src/components/app/PageHeader.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import GoToHomeScreenButton from "./GoToHomeScreenButton";
 
 interface PageHeaderProps extends React.PropsWithChildren {
   title: string;
+  showBackAction?: boolean;
 }
 
-function PageHeader({ title, children }: PageHeaderProps) {
+function PageHeader({
+  title,
+  children,
+  showBackAction = false,
+}: PageHeaderProps) {
   return (
     <div className="border-b border-border flex justify-between items-center px-6 py-4">
-      <h1 className="font-heading text-base font-medium">{title}</h1>
+      <div className="flex gap-2 items-center">
+        {showBackAction && <GoToHomeScreenButton variant="icon" />}
+        <h1 className="font-heading text-base font-medium">{title}</h1>
+      </div>
       {children}
     </div>
   );
