6027a5d74682f81ca814df9d05e1cb6665bf3f23
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2026-07-23 02:00:24 -0700

    add action buttons to create workspace header

--- src/screens/CreateWorkspaceScreen.tsx
+++ src/screens/CreateWorkspaceScreen.tsx
@@ -1,9 +1,20 @@
 import PageHeader from "@/components/app/PageHeader";
+import { Button } from "@/components/ui/button";
+import { SaveIcon } from "lucide-react";
 
 function CreateWorkspaceScreen() {
   return (
     <main>
-      <PageHeader title="Create Workspace" />
+      <PageHeader title="Create Workspace">
+        <div className="flex gap-4">
+          <Button type="button" variant={"outline"}>
+            Cancel
+          </Button>
+          <Button type="button">
+            <SaveIcon /> Save Workspace
+          </Button>
+        </div>
+      </PageHeader>
     </main>
   );
 }
