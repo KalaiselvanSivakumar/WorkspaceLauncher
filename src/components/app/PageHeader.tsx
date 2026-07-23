d67d6acb388d7925aa0f96cca3904c87836e8b2d
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2026-07-23 01:59:53 -0700

    fix header text alignment when actions items are present

--- src/components/app/PageHeader.tsx
+++ src/components/app/PageHeader.tsx
@@ -6,7 +6,7 @@ interface PageHeaderProps extends React.PropsWithChildren {
 
 function PageHeader({ title, children }: PageHeaderProps) {
   return (
-    <div className="border-b border-border flex justify-between px-6 py-4">
+    <div className="border-b border-border flex justify-between items-center px-6 py-4">
       <h1 className="font-heading text-base font-medium">{title}</h1>
       {children}
     </div>
