2fecfa07347d738ccb473aca922634659877db39
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2026-07-23 02:41:59 -0700

    add actions to configure workspace screen

--- src/screens/ConfigureWorkspaceScreen.tsx
+++ src/screens/ConfigureWorkspaceScreen.tsx
@@ -1,12 +1,26 @@
+import HeaderActions from "@/components/app/HeaderActions";
 import PageHeader from "@/components/app/PageHeader";
 import { ConfigureScreen, useUIStore } from "@/stores/ui-store";
+import { useCallback } from "react";
 
 function ConfigureWorkspaceScreen() {
   const screen = useUIStore((state) => state.screen) as ConfigureScreen;
 
+  const handlePrimaryAction = useCallback(function () {
+    console.log("Primary Action is clicked");
+  }, []);
+
   return (
     <main>
-      <PageHeader title={`Edit ${screen.workspaceName} Workspace`} />
+      <PageHeader
+        title={`Configure ${screen.workspaceName} Workspace`}
+        showBackAction
+      >
+        <HeaderActions
+          actionButtonText="Save"
+          handleAction={handlePrimaryAction}
+        />
+      </PageHeader>
     </main>
   );
 }
