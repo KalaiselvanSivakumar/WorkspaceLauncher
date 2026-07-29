8b43e37d078f79bfa43a485d2ea1f988232783ee
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2026-07-29 01:24:55 -0700

    show vs code launcher info in view mode

--- src/components/app/launcher/view/VisualStudioCodeLauncher.tsx
+++ src/components/app/launcher/view/VisualStudioCodeLauncher.tsx
@@ -1,5 +1,14 @@
 import { VsCodeLauncher } from "@/types/models";
 import ViewFieldValuePair from "./ViewFieldValuePair";
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item";
+import { VS_CODE_MAX_PROJECTS } from "@/utils/launchers";
+import { FolderIcon } from "lucide-react";
 
 interface VisualStudioCodeLauncherProps {
   launcher: VsCodeLauncher;
@@ -7,8 +16,28 @@ interface VisualStudioCodeLauncherProps {
 
 function VisualStudioCodeLauncher({ launcher }: VisualStudioCodeLauncherProps) {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <ViewFieldValuePair fieldName="Action" value={launcher.action} />
+      <Item className="p-0">
+        <ItemContent className="flex-row">
+          <ItemTitle>Project Folders</ItemTitle>
+          <ItemDescription>
+            ({launcher.path ? 1 : 0} / {VS_CODE_MAX_PROJECTS})
+          </ItemDescription>
+        </ItemContent>
+        <Item variant={"outline"} className="p-2">
+          {launcher.path ? (
+            <>
+              <ItemMedia variant={"icon"}>
+                <FolderIcon />
+              </ItemMedia>
+              <ItemContent>{launcher.path}</ItemContent>
+            </>
+          ) : (
+            <ItemContent>No Projects Configured</ItemContent>
+          )}
+        </Item>
+      </Item>
     </div>
   );
 }
