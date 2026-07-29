4083e2a0aff7c7c723573418aac86d2f6ca5b4fc
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2026-07-29 01:25:43 -0700

    use workspace form for both create and edit

--- src/screens/ConfigureWorkspaceScreen.tsx
+++ src/screens/ConfigureWorkspaceScreen.tsx
@@ -1,4 +1,4 @@
-import WorkspaceConfiguration from "@/components/app/workspace/configure/WorkspaceConfiguration";
+import WorkspaceForm from "@/components/app/workspace/configure/WorkspaceForm";
 import { useAppStore } from "@/stores/app-store";
 import { ConfigureScreen, useUIStore } from "@/stores/ui-store";
 import { useCallback } from "react";
@@ -6,14 +6,27 @@ import { useCallback } from "react";
 function ConfigureWorkspaceScreen() {
   const screen = useUIStore((state) => state.screen) as ConfigureScreen;
   const appData = useAppStore((state) => state.data.data);
-  const clonedWorkspaceConfig = structuredClone(appData?.find((workspace) => workspace.name === screen.workspaceName));
+  const clonedWorkspaceConfig = structuredClone(
+    appData?.find((workspace) => workspace.name === screen.workspaceName),
+  );
 
   const handlePrimaryAction = useCallback(function () {
     console.log("Primary Action is clicked");
   }, []);
 
   return (
-    <WorkspaceConfiguration text={{ pageTitle: `Configure "${screen.workspaceName}" Workspace`, primaryActionText: "Save" }} handlePrimaryAction={handlePrimaryAction} workspaceConfig={clonedWorkspaceConfig} />
+    // <WorkspaceConfiguration text={{ pageTitle: `Configure "${screen.workspaceName}" Workspace`, primaryActionText: "Save" }} handlePrimaryAction={handlePrimaryAction} workspaceConfig={clonedWorkspaceConfig} />
+    <WorkspaceForm
+      text={{
+        pageTitle: `Configure "${screen.workspaceName}" Workspace`,
+        pageDescription: "Modify your launcher configurations.",
+        primaryActionText: "Update",
+        submittingFormText: "Updating workspace...",
+      }}
+      initialData={{ id: "sdfhaksdjfh", name: "sdkakhjfadgi", launchers: [] }}
+      onCancel={() => {}}
+      onSuccess={() => {}}
+    />
   );
 }
 
--- src/screens/CreateWorkspaceScreen.tsx
+++ src/screens/CreateWorkspaceScreen.tsx
@@ -1,6 +1,6 @@
 import { Launcher } from "@/types/models";
 import { useCallback, useState } from "react";
-import WorkspaceConfiguration from "@/components/app/workspace/configure/WorkspaceConfiguration";
+import WorkspaceForm from "@/components/app/workspace/configure/WorkspaceForm";
 
 function CreateWorkspaceScreen() {
   const [launchers, setLaunchers] = useState<Launcher[]>([]);
@@ -10,7 +10,17 @@ function CreateWorkspaceScreen() {
   }, []);
 
   return (
-    <WorkspaceConfiguration text={{ pageTitle: "Create Workspace", primaryActionText: "Create" }} launchers={launchers} handlePrimaryAction={handlePrimaryAction} />
+    // <WorkspaceConfiguration text={{ pageTitle: "Create Workspace", primaryActionText: "Create" }} launchers={launchers} handlePrimaryAction={handlePrimaryAction} />
+    <WorkspaceForm
+      text={{
+        pageTitle: "Create Workspace",
+        primaryActionText: "Create",
+        pageDescription: "Configure launchers and paths for your workspace.",
+        submittingFormText: "Creating workspace...",
+      }}
+      onCancel={function () {}}
+      onSuccess={function () {}}
+    />
   );
 }
 
